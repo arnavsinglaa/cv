c3223651bbfd02e79864299a41f0618238f85f86
arnavsinglaa <singlaarnav@gmail.com>
2024-06-12 00:05:38 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const candles = document.querySelectorAll('.candle');
-    candles.forEach(candle => {
-        candle.addEventListener('click', function () {
-            this.classList.toggle('blow');
-        });
-    });
-});
